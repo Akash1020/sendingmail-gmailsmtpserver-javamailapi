077a612c367c5309d0a89db96ca5a599bcd484ba
Giau Ngo <giaunv@users.noreply.github.com>
2017-12-19 03:43:15 +0700

    clean

--- src/main/java/com/hellokoding/mail/Main.java
+++ src/main/java/com/hellokoding/mail/Main.java
@@ -7,6 +7,6 @@ public class Main {
         String toUserEmail = args[2];
 
         SendingMailThroughGmailSMTPServer sendingMailThroughGmailSMTPServer = new SendingMailThroughGmailSMTPServer();
-        sendingMailThroughGmailSMTPServer.sendMail(fromUserEmail, fromUserAccessToken, toUserEmail, "Sending Email Through Gmail SMTP Server with Java Mail API and OAuth 2 Authorization", "Hi Giau,\n\rThis is a programmatic email.");
+        sendingMailThroughGmailSMTPServer.sendMail(fromUserEmail, fromUserAccessToken, toUserEmail, "Sending Email Through Gmail SMTP Server with Java Mail API and OAuth 2 Authorization", "Hi,\n\rThis is a programmatic email.");
     }
 }
